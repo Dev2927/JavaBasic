f73cd1b0721b482925cde31407fc23aba6e41abd
dev2927 <devanand290702@gmail.com>
2024-03-23 22:29:20 +0530

    Added into literals in variables

---
+++ Variable/src/Variable/Literals.java
@@ -0,0 +1,17 @@
+package Variable;
+
+public class Literals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte age = 34;
+		int age2 = 48;
+		short age3 = 4;
+		long lg = 4567890;
+		char ch = 'S';
+		float f1 = 38.3F;
+		double d1 = 543.3;
+		boolean a = true;
+	}
+
+}
